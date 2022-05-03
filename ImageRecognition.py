c0d17a40281be0e273877fceccdb61b0e9bce60e
SantiagoLopez-hub <silp4477@gmail.com>
2022-05-03 21:02:03 +0100

    Fix spacing

--- ImageRecognition.py
+++ ImageRecognition.py
@@ -1,44 +1,39 @@
 import tensorflow as tf
 from keras.preprocessing.image import ImageDataGenerator
 
-
 print(tf.__version__)
 
-
-# Pre processing training set
+# Pre-processing training set
 train_datagen = ImageDataGenerator(
-	rescale=1./255,
-	shear_range=0.2,
-	zoom_range=0.2,
-	horizontal_flip=True)
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
 
 training_set = train_datagen.flow_from_directory(
-	'dataset/training_set',
-	target_size=(64, 64),
-	batch_size=32,
-	class_mode='binary')
+    'dataset/training_set',
+    target_size=(64, 64),
+    batch_size=32,
+    class_mode='binary')
 
-
-# Pre processing testing set
-test_datagen = ImageDataGenerator(rescale=1./255)
+# Pre-processing testing set
+test_datagen = ImageDataGenerator(rescale=1. / 255)
 
 testing_set = test_datagen.flow_from_directory(
-	'dataset/test_set',
-	target_size=(64, 64),
-	batch_size=32,
-	class_mode='binary')
-
+    'dataset/test_set',
+    target_size=(64, 64),
+    batch_size=32,
+    class_mode='binary')
 
 # Initialising Convolutional Neural Network
 cnn = tf.keras.models.Sequential()
 
 # Convolution
 cnn.add(tf.keras.layers.Conv2D(
-	filters=32,
-	kernel_size=3,
-	activation='relu',
-	input_shape=[64,64,3]))
-
+    filters=32,
+    kernel_size=3,
+    activation='relu',
+    input_shape=[64, 64, 3]))
 
 # Pooling
 cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
