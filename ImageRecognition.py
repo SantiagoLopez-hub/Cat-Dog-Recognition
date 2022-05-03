eb80436f524f6b2a7a09143c8cb3afe647858dba
SantiagoLopez-hub <silp4477@gmail.com>
2022-05-03 19:59:22 +0100

    Data generation

--- ImageRecognition.py
+++ ImageRecognition.py
@@ -4,3 +4,10 @@ from keras.preprocessing.image import ImageDataGenerator
 
 print(tf.__version__)
 
+
+train_datagen = ImageDataGenerator(
+	rescale=1./255,
+	shear_range=0.2,
+	zoom_range=0.2,
+	horizontal_flip=True)
+
