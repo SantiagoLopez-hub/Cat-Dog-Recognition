cc09b6fdb5ff568b8f345b2d66470df6b86a26a9
SantiagoLopez-hub <silp4477@gmail.com>
2022-05-03 20:06:44 +0100

    Adding Pre processing testing set

--- ImageRecognition.py
+++ ImageRecognition.py
@@ -5,6 +5,7 @@ from keras.preprocessing.image import ImageDataGenerator
 print(tf.__version__)
 
 
+# Pre processing training set
 train_datagen = ImageDataGenerator(
 	rescale=1./255,
 	shear_range=0.2,
@@ -13,6 +14,16 @@ train_datagen = ImageDataGenerator(
 
 training_set = train_datagen.flow_from_directory(
 	'dataset/training_set',
+	target_size=(64, 64),
+	batch_size=32,
+	class_mode='binary')
+
+
+# Pre processing testing set
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+testing_set = test_datagen.flow_from_directory(
+	'dataset/test_set',
 	target_size=(64, 64),
 	batch_size=32,
 	class_mode='binary')
