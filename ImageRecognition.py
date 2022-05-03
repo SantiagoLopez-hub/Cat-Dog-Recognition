66884c4678df6cd3fad6751ce467edfeea69429d
SantiagoLopez-hub <silp4477@gmail.com>
2022-05-03 19:59:29 +0100

    Training set

--- ImageRecognition.py
+++ ImageRecognition.py
@@ -11,3 +11,8 @@ train_datagen = ImageDataGenerator(
 	zoom_range=0.2,
 	horizontal_flip=True)
 
+training_set = train_datagen.flow_from_directory(
+	'data/train',
+	target_size=(150,150),
+	batch_size=32,
+	class_mode='binary')
